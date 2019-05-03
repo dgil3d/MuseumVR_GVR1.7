00ca629d4cb2bbb545eae0886070d0087286658f
dgil3d <diegogile@gmail.com>
2019-05-03 15:39:04 -0400

    Fifth Commit
    Updated files and README to match origin.

---
+++ Assets/Scripts/MuseumLogic.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MuseumLogic : MonoBehaviour {
+
+    public GameObject player;
+    public GameObject eventSystem;
+    //public GameObject startUI, restartUI;
+    //public GameObject startPoint, playPoint, restartPoint;
+    public GameObject[] stations; //An array to hold our puzzle spheres
+
+	// Use this for initialization
+	void Start () {
+		
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	
+}
